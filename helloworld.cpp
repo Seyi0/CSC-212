428844407e35a1bae3891f8bc0d874ad1fa0337a
Seyi0 <makanjuolaseyi560@gmail.com>
2023-06-12 02:36:25 +0100

    first commit

---
+++ helloworld.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+int main(){
+
+    //This is a comment
+        /* take good note of the following used in c++
+                std-standard
+                c-character
+                out-ouput
+                endl- end line
+                \n - new line
+                double - number including decimal
+                in a single character make use of single quote
+        */
+
+
+
+
+            // std::cout << "I Love Coding!" << '\n';
+            // std::cout<<"It's amazing!"<< std:: endl;
+
+             int x = 5; 
+             int y = 7;
+             int sum = x + y ;
+
+             double gpa = 3.6;
+
+             // single character
+
+             char grade = 'A';
+
+             std :: cout << x <<'\n';
+             std :: cout << y  <<'\n';
+             std :: cout<< sum<<'\n';
+             std :: cout <<gpa << '\n';
+             std :: cout <<grade<<'\n';
+
+
+    return 0;
+}
